54f34214ec1da1e606961f533e2baeb4b9e74d64
yeabsra-leul <74534830+yeabsra-leul@users.noreply.github.com>
2024-02-23 00:36:17 +0300

    implement post feed

---
+++ src/components/Post.tsx
@@ -0,0 +1,69 @@
+import { User, Vote, Post } from '@prisma/client'
+import React, { FC, useRef } from 'react'
+import {formatTimeToNow} from "@/lib/utils"
+import Link from 'next/link'
+import { MessageSquare } from 'lucide-react'
+import EditorOutput from './EditorOutput'
+
+interface PostProps {
+    post: Post & {
+      author: User
+      votes: Vote[]
+    }
+    subredditName:string,
+    commentAmt:number;
+    votesAmt:number;
+
+  }
+
+const Post:FC<PostProps> = ({post,subredditName, commentAmt, votesAmt}) => {
+
+    const pRef = useRef<HTMLParagraphElement>(null)
+    
+    return (
+        <div className='rounded-md bg-white shadow'>
+          <div className='px-6 py-4 flex justify-between'>
+          <div className='w-0 flex-1'>
+          <div className='max-h-40 mt-1 text-xs text-gray-500'>
+            {subredditName ? (
+              <>
+                <a
+                  className='underline text-zinc-900 text-sm underline-offset-2'
+                  href={`/r/${subredditName}`}>
+                  r/{subredditName}
+                </a>
+                <span className='px-1'>•</span>
+              </>
+            ) : null}
+            <span>Posted by u/{post.author.username}</span>{' '}
+            {formatTimeToNow(new Date(post.createdAt))}
+          </div>
+          <a href={`/r/${subredditName}/post/${post.id}`}>
+            <h1 className='text-lg font-semibold py-2 leading-6 text-gray-900'>
+              {post.title}
+            </h1>
+          </a>
+
+          <div
+            className='relative text-sm max-h-40 w-full overflow-clip'
+            ref={pRef}>
+            <EditorOutput content={post.content} />
+            {pRef.current?.clientHeight === 160 ? (
+              <div className='absolute bottom-0 left-0 h-24 w-full bg-gradient-to-t from-white to-transparent'></div>
+            ) : null}
+          </div>
+        </div>
+      </div>
+
+      <div className='bg-gray-50 z-20 text-sm px-4 py-4 sm:px-6'>
+        <Link
+          href={`/r/${subredditName}/post/${post.id}`}
+          className='w-fit flex items-center gap-2'>
+          <MessageSquare className='h-4 w-4' /> {commentAmt} comments
+        </Link>
+      </div>
+      </div> 
+    )
+}
+
+export default Post
--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SessionProvider } from "next-auth/react";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   const queryClient = new QueryClient();
 
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+  return <QueryClientProvider client={queryClient}><SessionProvider>{children}</SessionProvider></QueryClientProvider>;
 };
