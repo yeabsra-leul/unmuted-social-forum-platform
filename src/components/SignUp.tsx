d389e98b208379cdb288f35c5c0ed7eb112eebf7
yeabsra-leul <74534830+yeabsra-leul@users.noreply.github.com>
2024-03-22 16:25:21 +0300

    updated

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -6,7 +6,7 @@ const SignUp = () => {
   return (
     <div className='container mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px]'>
       <div className='flex flex-col space-y-2 text-center'>
-        <Icons.logo className='mx-auto h-6 w-6' />
+        <Icons.waveLogo className='mx-auto h-6 w-6' />
         <h1 className='text-2xl font-semibold tracking-tight'>Sign Up</h1>
         <p className='text-sm max-w-xs mx-auto'>
           By continuing, you are setting up an Unmuted account and agree to our
