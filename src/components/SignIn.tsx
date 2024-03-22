bf6b398a76d6a42acd8fb14797383bf9787b3094
yeabsra-leul <74534830+yeabsra-leul@users.noreply.github.com>
2024-03-22 16:40:20 +0300

    updated logo alignment

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -6,7 +6,9 @@ const SignIn = () => {
   return (
     <div className='container mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px]'>
       <div className='flex flex-col space-y-2 text-center'>
-        <Icons.waveLogo className='mx-auto h-6 w-6 text-center' />
+      <div className="flex justify-center ">
+      <Icons.waveLogo className='mx-auto h-6 w-6' />
+      </div>
         <h1 className='text-2xl font-semibold tracking-tight'>Welcome back</h1>
         <p className='text-sm max-w-xs mx-auto'>
           By continuing, you are setting up an Unmuted account and agree to our
