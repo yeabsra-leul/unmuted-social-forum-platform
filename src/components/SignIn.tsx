0aa40926503dc303c05e19d154969d28f559de8c
yeabsra-leul <74534830+yeabsra-leul@users.noreply.github.com>
2024-03-22 16:29:11 +0300

    updated logo

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -6,7 +6,7 @@ const SignIn = () => {
   return (
     <div className='container mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px]'>
       <div className='flex flex-col space-y-2 text-center'>
-        <Icons.waveLogo className='mx-auto h-6 w-6' />
+        <Icons.waveLogo className='mx-auto h-6 w-6 text-center' />
         <h1 className='text-2xl font-semibold tracking-tight'>Welcome back</h1>
         <p className='text-sm max-w-xs mx-auto'>
           By continuing, you are setting up an Unmuted account and agree to our
@@ -15,7 +15,7 @@ const SignIn = () => {
       </div>
       <UserAuthForm />
       <p className='px-8 text-center text-sm text-muted-foreground'>
-        New to Breaddit?{' '}
+        New to Unmuted?{' '}
         <Link
           href='/sign-up'
           className='hover:text-brand text-sm underline underline-offset-4'>
